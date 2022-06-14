17266f16d3cd22de3a310ab4fc018cb970fc4945
Sergey Kovalev <dip.deep@mail.ru>
2022-06-14 21:37:45 +0300

    first commit

---
+++ src/main/java/ru/sf/SimpleWebDemo/SimpleWebDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.sf.SimpleWebDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sf/SimpleWebDemo/SimpleWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sf.SimpleWebDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
